0310006186e80f6d22cd2a887a20b30d0b416e21
Henry Eriksson <eriksson.henry@elev.ga.ntig.se>
2021-11-30 14:40:33 +0100

    Initial Code

---
+++ cringe/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+string room = "mancave";
+
+while (room != "exit")
+{
+    if(room == "mancave"){
+        Console.WriteLine("you are in your mancave");
+        string action = Console.ReadLine();
+        if(action == "classroom"){
+            room = "classroom";
+        } else if (action == "lab") {
+            Console.WriteLine("you are in the lab. go home? exit / stay");
+            action = Console.ReadLine();
+            if (action == "exit"){
+                room = "mancave";
+            } else {action = "lab";}
+        }
+    } else if (room == "classroom"){
+        while(room != "lab" || room != "mancave"){
+            Console.WriteLine("you are in the classroom");
+            action = Console.WriteLine();
+        }
+    }
+}
